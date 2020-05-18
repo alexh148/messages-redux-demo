f763767bb04a350d2419367a7fd96eace7b27b01
Alex Hutchings <alexhutchings4@gmail.com>
2020-05-18 20:54:04 +0100

    Sets index comments right way around

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import App from './components/App';
+import App from './components/App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import App2Container from "./components/App2Container";
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App2Container />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  rootElement
+  document.getElementById('root')
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+// import App2Container from "./components/App2Container";
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App2Container />
+//     </Provider>
+//   </React.StrictMode>,
+//   rootElement
+// );
